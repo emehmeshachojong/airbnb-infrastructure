ce532e5b93523b66a3c03f02ffcb9180d31801ca
emeh meshach <emehmeshachojong@gmail.com>
2023-06-14 18:23:46 -0500

    first commit of infrastructure

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "AMI_ID" {
     type = string
     description = "AMI ID"
-    default = "ami-0149b2da6ceec4bb0"
+    default = "ami-09988af04120b3591"
 }
 
 variable "INSTANCE_TYPE" {
     type = string
     description = "type of EC2 instance"
-    default = "t3.micro"
+    default = "t2.micro"
 }
